e4bf7d923f9bd559738d43d60a98212b0cbd647d
benleamon <ben.leamon@gmail.com>
2026-05-10 16:29:51 +0900

    Update path for icon

--- qgis_blender.py
+++ qgis_blender.py
@@ -181,7 +181,7 @@ class QgisBlender:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/qgis_blender/icon.png'
+        icon_path = f'{self.plugin_dir}/icon.png'
         self.add_action(
             icon_path,
             text=self.tr(u'Prepare DEMs for Blender'),
